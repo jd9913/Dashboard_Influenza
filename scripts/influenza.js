a28fe879cc76aec967520be8803f2775313abcf5
Jennifer Schlierman <jd9913@gmail.com>
2020-11-17 12:57:28 -0700

    updates

--- scripts/influenza.js
+++ scripts/influenza.js
@@ -11,7 +11,6 @@ const getData = () => {
 getData();
 
 
-
 let lineED = []; // array to hold data for ED line on line chart
 let lineVent = []; //array to hold data for vent in use line on line chart
 let lineInpt = []; //array to hold data for inpatients line on line chart
@@ -19,10 +18,10 @@ let lineIcu = []; //array to hold data for icu line on line chart
 
 //variables for the colors on the charts
 
-const color1 = '#FF00FF';
-const color2 = '#00FACC';
-const color3 = '#FFCC00';
-const color4 = '#CCFF00';
+const color3 = '#590995';
+const color2 = '#03C4A1';
+const color1 = '#C62A88';
+const color4 = '#150485';
 const fontColor = '#504F4F';
 
 //referencing the DOM to place the line chart in the appropriate place
@@ -31,10 +30,10 @@ const fluLineEl = document.getElementById('fluLine');
 //variables for the labels on the line chart
 
 let xAxisLabels = []; //variable for the x axis labels
-let labelED = ['ED pos/susp influenza'];
-let labelVent = ['Vent pos/susp influenza'];
-let labelInpt = ['inpatients pos/susp influenza'];
-let labelIcu = ['ICU pos/susp influenza'];
+let labelED = ['ED'];
+let labelVent = ['Vents'];
+let labelInpt = ['Inpatients'];
+let labelIcu = ['ICU'];
 
 //put current date in the header near the title;
 
@@ -52,12 +51,11 @@ function getCurrentDay() {
 getCurrentDay();
 
 
-
 function graph(data1) {
 
     //adding the date filter to only pull dates with valid data
 
-    let dateFilter = '10/01/2020';
+    let dateFilter = '10/28/2020';
 
    $('#asOfDate').text(dateFilter);
 
